7b3466c0e067e0d5066c8ddf79b629d98f18e309
Ayush <ayushupadhyay456@gmail.com>
2022-05-31 23:05:37 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div><h1 className='text-center'>All Items</h1>
+    <section className="py-4 container">
+        <div className="row justify-content"></div>
+    </section>
+    </div>
+  )
+}
+
+export default Home
